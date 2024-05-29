288c86ab7f10e0936a9c45f3ad8029d55564aa6f
Naufal Azhar <naufalazhar65@gmail.com>
2024-05-29 14:30:21 +0700

    update

--- target/test-classes/Features/Login.feature
+++ target/test-classes/Features/Login.feature
@@ -16,7 +16,7 @@ Feature: Login Functionality Test
       | username      | password     |
       | standard_user | secret_sauce |
 
-  #
+  
   Scenario Outline: Login with invalid account
     When I enter username "<username>" and password "<password>"
     And I click the login button
---
+++ test.groovy
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+
+    environment {
+        MAVEN_HOME = '/opt/homebrew' // Base directory where Maven is installed
+        PATH = "${MAVEN_HOME}/bin:${env.PATH}"
+    }
+
+    stages {
+        stage('Get Source Code') {
+            steps {
+                git branch: 'main', credentialsId: '1b0cd892-6612-4b85-b3b0-8d8f9d34d5b9', url: 'https://github.com/naufalazhar65/SELENIUM-JAVA-BDD-POM'
+                echo 'Hello World'
+            }
+        }
+        stage('Verify Maven') {
+            steps {
+                sh 'echo $PATH'
+                sh 'mvn -v'
+            }
+        }
+        stage('Build Code') {
+            steps {
+                sh 'mvn compile'
+            }
+        }
+        stage('Run Test') {
+            steps {
+                sh 'mvn clean test'
+            }
+        }
+        stage('Publish report') {
+            steps {
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: '', reportFiles: 'target/surefire-reports/index.html', reportName: 'Pipeline', reportTitles: '', useWrapperFileDirectly: true])
+                testNG()
+            }
+        }
+    }
+}
